cb0e7401b5c2c1d237bcb68310877a4c210216f9
han122400 <vinny122400@gmail.com>
2025-05-12 09:32:01 +0900

    2

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 // index.html의 JavaScript 코드
 
 function login() {
-  const id = document.getElementById('id').value;
-  const pw = document.getElementById('pw').value;
+  const id = document.getElementById('id').value
+  const pw = document.getElementById('pw').value
 
   if (localStorage.getItem(id) === pw) {
-    let username = id; // 로그인 성공 시 name 변수에 사용자 ID 저장
-    document.getElementById('output').innerText = '✅ 로그인 성공!';
+    let username = id // 로그인 성공 시 name 변수에 사용자 ID 저장
+    document.getElementById('output').innerText = '✅ 로그인 성공!'
 
     //쿠키로 전달
     //document.cookie = `username=${username}; path=/; max-age=3600;`;
 
     //URL로 전달
-    location.href = `login.html?name=${encodeURIComponent(username)}`;
+    location.href = `login.html?name=${encodeURIComponent(username)}`
   } else {
-    document.getElementById('output').innerText = '❌ 로그인 실패!';
+    document.getElementById('output').innerText = '❌ 로그인 실패!'
   }
 }
 
@@ -23,11 +23,11 @@ function sign() {
   localStorage.setItem(
     document.getElementById('id').value,
     document.getElementById('pw').value
-  );
-  document.getElementById('output').innerText = '✅ 회원가입 완료!';
-  location.href = 'index.html';
+  )
+  document.getElementById('output').innerText = '✅ 회원가입 완료!'
+  location.href = 'index.html'
 }
 
 // login.html의 JavaScript 코드
 
-//페이지간의 데이터 전달은 URL,쿠키,웹 스토리지 가 있음
+//페이지간의 데이터 전달은 URL,쿠키,웹 스토리지 가있음
